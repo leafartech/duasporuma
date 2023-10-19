7f72026e2137c4fc7bf7b0f7f171e6700b7e15af
leafartech <119262053+leafartech@users.noreply.github.com>
2023-10-19 15:29:31 -0300

    Update index.tsx

--- src/components/HeroFlex/index.tsx
+++ src/components/HeroFlex/index.tsx
@@ -13,6 +13,9 @@ export default function HeroFlex({ children, imagePath, reverse, title }: HeroFl
             <div className="flex flex-col gap-2 sm:gap-5 max-w-[520px]">
                 <h2 className="font-bold text-xl sm:text-[30px] sm:leading-10">{title}</h2>
                 { children }
+                <div className="">
+                    <a href="#forms" className="bg-my text-white px-6 py-3 font-medium text-center rounded-lg sm:rounded-xl">Quero visitar o Brasil quando quiser!</a>
+                </div>
             </div>
             <div className="sm:w-420 lg:w-[520px] flex justify-end mt-4 sm:mt-0">
                 <img src={`../images/${imagePath}`} alt="Imagem Hero" className="sm:h-full" />
