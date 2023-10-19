a40163c146138f5ed21525f062b05c1b474a9826
leafartech <119262053+leafartech@users.noreply.github.com>
2023-10-19 19:20:56 -0300

    Delete src directory

--- src/components/Footer/index.tsx
+++
@@ -1,8 +0,0 @@
-export default function Footer() {
-    return (
-        <footer className="bg-my text-center text-white text-sm py-4 px-4">
-            <p>Copyright © 2023 | Treinamento - Duas Por Uma</p>
-            <p>Sede de Viajar Comércio de Infoprodutos e Cursos Digitais LTDA - 47.418.377/0001-29</p>
-        </footer>
-    )
-}
